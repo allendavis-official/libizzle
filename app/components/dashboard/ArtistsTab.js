9368accca23ac2bcf73189134823bf47df6357c8
allendavis-official <allendavis_official@hotmail.com>
2025-10-11 13:03:11 +0000

    escaped quoation mark in ArtistsTab.js

--- app/components/dashboard/ArtistsTab.js
+++ app/components/dashboard/ArtistsTab.js
@@ -90,7 +90,8 @@ export default function ArtistsTab({
       {searchTerm && (
         <div className="text-xs sm:text-sm text-gray-300">
           Found {sortedArtists.length} artist
-          {sortedArtists.length !== 1 ? "s" : ""} matching "{searchTerm}"
+          {sortedArtists.length !== 1 ? "s" : ""} matching &quot;{searchTerm}
+          &quot;
         </div>
       )}
 
